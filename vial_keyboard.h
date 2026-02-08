9ae8dcf6b3e73575901a4fb72ad5bd8d4155a2d1
mmerlino-ai <mmerlino399@gmail.com>
2026-02-07 20:50:43 -0500

    Create vial_keyboard.h

---
+++ vial_keyboard.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "quantum.h"
+
+// random but valid UID – you can change if you like
+#define VIAL_KEYBOARD_UID { 0xA3, 0x5F, 0x92, 0x1C, 0x47, 0xD8, 0x6B, 0xE0 }
+
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 0 } // "1" and "TAB"
