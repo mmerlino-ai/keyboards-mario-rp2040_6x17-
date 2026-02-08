0011403f1662a5fa704857868385df8775ee1980
mmerlino-ai <mmerlino399@gmail.com>
2026-02-07 20:49:51 -0500

    Create config.h

---
+++ config.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "config_common.h"
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Mario
+#define PRODUCT         Mario_RP2040_6x17
+
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 17
+
+#define MATRIX_ROW_PINS { GP2, GP3, GP4, GP5, GP6, GP7 }
+#define MATRIX_COL_PINS { GP8, GP9, GP10, GP11, GP12, GP13, GP14, GP15, GP16, GP17, GP18, GP19, GP20, GP21, GP22, GP26, GP27 }
+
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE 5
+#define USB_POLLING_INTERVAL_MS 1
+
+#define LOCKING_SUPPORT_ENABLE
+#define LOCKING_RESYNC_ENABLE
+
+#define TAP_CODE_DELAY 10
